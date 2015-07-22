050aa4a23ed0e5d9eed2eeeef2e3eae9abffa41e
Mark Lopez <marklopez@Marks-MacBook-Pro.local>
2015-07-21 21:08:43 -0400

    styles and japan and here

--- app/views.py
+++ app/views.py
@@ -4,13 +4,31 @@ import requests
 import json
 
 @app.route('/')
-@app.route('/index')
+@app.route('/here')
 def index():
 	send_url = 'http://freegeoip.net/json'
 	r = requests.get(send_url)
 	j = json.loads(r.text)
 	lat = j['latitude']
 	log = j['longitude']
+	url = "http://api.openweathermap.org/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(log) + "&APPID=b1bf90077b688b5e1b3c75301de15520"
+	response = requests.get(url).json()
+	weather = response['weather'][0]['main']
+
+	if weather == "Rain":
+		isitraining  = "yes"
+	else:
+		isitraining  = "no"
+  
+	return render_template("index.html",lat=lat,log=log,isitraining=isitraining)
+
+@app.route('/japan')
+def test():
+	send_url = 'http://freegeoip.net/json'
+	r = requests.get(send_url)
+	j = json.loads(r.text)
+	lat = 45
+	log = 139
 	url = "http://api.openweathermap.org/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(log) + "&APPID=b1bf90077b688b5e1b3c75301de15520"
 	response = requests.get(url).json()
 	weather = response['weather'][0]['main']
