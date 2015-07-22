9af35529f6ed5d4ba11710ebce55a92def055156
Dave Rooney <drooney@gqrr.com>
2015-07-21 23:37:00 -0400

    Removed API key

--- rain.py
+++ rain.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://api.openweathermap.org/data/2.5/weather?lat=45&lon=139&APPID=b1bf90077b688b5e1b3c75301de15520"
+url = "http://api.openweathermap.org/data/2.5/weather?lat=45&lon=139"
 
 response = requests.get(url).json()
 weather = response['weather'][0]['main']
